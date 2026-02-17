2463d26c4a26d27bacf95b33f952b5dfdb335c1f
Dmitry Verkhoturov <paskal.07@gmail.com>
2026-02-17 19:40:25 +0000

    Add shell completions for bash, zsh, and fish

---
+++ completions/weblist.zsh
@@ -0,0 +1,15 @@
+#compdef weblist
+
+# zsh completion for weblist (generated via go-flags)
+_weblist() {
+    local IFS=$'\n'
+    local -a completions
+    completions=($(GO_FLAGS_COMPLETION=1 "${words[1]}" "${(@)words[2,$CURRENT]}" 2>/dev/null))
+    if (( ${#completions} )); then
+        compadd -- "${completions[@]}"
+    else
+        _files
+    fi
+}
+
+_weblist "$@"
