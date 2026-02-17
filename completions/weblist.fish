4f09aade08f8c83835d6605b56815c7094dcc8d9
Dmitry Verkhoturov <paskal.07@gmail.com>
2026-02-17 20:11:33 +0000

    use verbose completions for zsh and fish descriptions

--- completions/weblist.fish
+++ completions/weblist.fish
@@ -1,2 +1,2 @@
 # fish completion for weblist (generated via go-flags)
-complete -c weblist -a '(GO_FLAGS_COMPLETION=1 weblist (commandline -cop) 2>/dev/null)'
+complete -c weblist -a '(GO_FLAGS_COMPLETION=verbose weblist (commandline -cop) 2>/dev/null | string replace -r "\\s+# " "\t")'
